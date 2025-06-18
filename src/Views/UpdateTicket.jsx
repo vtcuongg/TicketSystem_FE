d906ffb5d4fae394ec681e7d72afc1619b849f23
cuong <cuong@gmail.com>
2025-06-18 23:16:12 +0700

    1

--- src/Views/UpdateTicket.jsx
+++ src/Views/UpdateTicket.jsx
@@ -397,7 +397,7 @@ const UpdateTicket = () => {
                                 value={formData?.Status}
                                 onChange={handleChange}
                                 required
-                                disabled={user?.roleName === "User"}
+                                readOnly={user?.roleName === "User"}
                             >
                                 <option value="">Select a Status</option>
                                 {StatusOptions.map((option) => (
